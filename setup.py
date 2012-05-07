3158c70810ba30d41e0f97d24919c0e72437d2e1
James Turnbull <james@incuna.com>
2012-05-07 21:32:42 +0100

    Added missing license file
    and tidied up setup.py

--- setup.py
+++ setup.py
@@ -3,18 +3,14 @@ from setuptools import find_packages, setup
 
 from orderable import get_version
 
-def fread(fname):
-    with open(join(dirname(__file__), fname), 'r') as f:
-        return f.read()
-
 setup(
     name = "django-orderable",
     packages = find_packages(),
     include_package_data=True,
     version = get_version(),
-    description = "",
+    description = "Orderable model and admin",
     author = "Incuna Ltd",
     author_email = "dev@incuna.com",
     url = "http://incuna.com/",
-    long_description=fread("README.rst"),
+    long_description=open("README.rst").read(),
 )
