6d70b0edc79c5509b7e65aa95c7b77274e984a21
Charlie Denton <charlie@meshy.co.uk>
2014-12-04 12:10:24 +0000

    Add license and classifiers to setup.py

--- setup.py
+++ setup.py
@@ -11,4 +11,15 @@ setup(
     author_email='dev@incuna.com',
     url='https://github.com/incuna/django-orderable',
     long_description=open('README.rst').read(),
+    license='BSD',
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Natural Language :: English',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+    ],
 )
