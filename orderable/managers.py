c63cf3972ecd26438c3825bdf67ebd0e8b4751f1
todd-dembrey <toddd@incuna.com>
2016-11-08 14:04:28 +0000

    update docstring formatting

--- orderable/managers.py
+++ orderable/managers.py
@@ -2,10 +2,11 @@ from django.db import models
 
 
 class OrderableManager(models.Manager):
-    '''
-    Adds additional functionality to `Orderable.objects` providing access to the next and
-    previous ordered object within the queryset.
-    '''
+    """
+    Adds additional functionality to `Orderable.objects`.
+
+    Provides access to the next and previous ordered object within the queryset.
+    """
     def before(self, orderable):
         return self.filter(sort_order__lt=orderable.sort_order).last()
 
