7501bbc4a2816c2dacdaa6af50379ef4cab3f8ca
Charlie Denton <charlie@meshy.co.uk>
2018-10-27 12:27:28 +0100

    Run the selenium tests in headless mode

--- features/browser.py
+++ features/browser.py
@@ -1,8 +1,13 @@
 from selenium import webdriver
 
 
+chrome_options = webdriver.chrome.options.Options()
+chrome_options.add_argument("--headless")
+chrome_options.add_argument("--window-size=1024x768")
+
+
 class Browser(object):
-    driver = webdriver.Chrome()
+    driver = webdriver.Chrome(chrome_options=chrome_options)
     driver.implicitly_wait(5)
 
     def close(context):
